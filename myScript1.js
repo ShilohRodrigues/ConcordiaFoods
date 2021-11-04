e2db50da029bebee6dd56f7ac2a1954a20565666
AdamFarahat <91172205+AdamFarahat@users.noreply.github.com>
2021-11-04 16:54:31 -0400

    Update myScript1.js

--- myScript1.js
+++ myScript1.js
@@ -1,20 +1,25 @@
 function updatePrice() {
-  
+
   let quantity = document.getElementById("quantity");
   let costPerKg = document.getElementById("cost-per-kg");
   let weight = document.getElementById("weight");
-  
+
   let totPrice;
-  
+
   //Check if the price is per kg or per unit
   //Weight will be null if it is per unit
-  if (weight == null) 
+  if (weight == null)
     totPrice = quantity.value * costPerKg.innerHTML;
-  else 
+  else
     totPrice = quantity.value * costPerKg.innerHTML * weight.innerHTML;
-  
+
   //Round to two decimals
   totPrice = Math.round(totPrice*100) / 100;
   document.getElementById("tot-price").innerHTML = "$" + totPrice;
-  
+  function storeQ(){
+    localStorage.setItem("store",document.getElementById("addToCart").value);
+  }
+  function getQ(){
+    document.getElementById("addToCart").value=localStorage.getItem("store");
+  }
 }
