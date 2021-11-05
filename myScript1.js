e9bfc10486a8b5bd5d3fd777b55a7c6de9d5fe7e
ShilohRodrigues <78390850+ShilohRodrigues@users.noreply.github.com>
2021-11-04 23:10:24 -0400

    fixed page refresh

--- myScript1.js
+++ myScript1.js
@@ -1,26 +1,27 @@
 function updatePrice() {
 
-  let quantity = document.getElementById("quantity");
+  let qt = document.getElementById("quantity");
   let costPerKg = document.getElementById("cost-per-kg");
   let weight = document.getElementById("weight");
-
   let totPrice;
 
   //Check if the price is per kg or per unit
   //Weight will be null if it is per unit
   if (weight == null)
-    totPrice = quantity.value * costPerKg.innerHTML;
+    totPrice = qt.value * costPerKg.innerHTML;
   else
-    totPrice = quantity.value * costPerKg.innerHTML * weight.innerHTML;
+    totPrice = qt.value * costPerKg.innerHTML * weight.innerHTML;
 
   //Round to two decimals
   totPrice = Math.round(totPrice*100) / 100;
   document.getElementById("tot-price").innerHTML = "$" + totPrice;
 
 } 
+
 function storeQ(){
   localStorage.setItem("store",document.getElementById("quantity").value);
 }
 function getQ(){
   document.getElementById("quantity").value=localStorage.getItem("store");
+  updatePrice();
 }
