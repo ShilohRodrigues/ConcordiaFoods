2653907a8324e00c603ed4a8f031664907816f0d
AdamFarahat <91172205+AdamFarahat@users.noreply.github.com>
2021-11-30 19:49:09 -0500

    Update Validate.php

--- FrontEndPages/Validate.php
+++ FrontEndPages/Validate.php
@@ -1,16 +1,18 @@
 <?php
+session_start();
 $string=file_get_contents("users.json");
 $users_a=json_decode($string,true);
 $check=false;
 
 foreach($users_a as $value){
     if($_POST['StudentID']==$value["StudentID"] and $_POST['Password']==$value["Password"]){
+
       $_SESSION['StudentID']=$_POST ['StudentID'];
-      header("location:../index.html");
+      header("location: ../index.html");
       $check=true;
     }
 }if(!$check){
-  $_SESSION["error"]="Username and/or Password is incorrect"
-  header("location:login.php");
+  $_SESSION["error"]="Username and/or Password is incorrect";
+  header("Location: login.php");
 }
 ?>
