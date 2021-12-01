f89549d34429265dd955e17b247d30fb450e8820
AdamFarahat <91172205+AdamFarahat@users.noreply.github.com>
2021-11-30 20:59:37 -0500

    Update _dbconnect.php

--- FrontEndPages/_dbconnect.php
+++ FrontEndPages/_dbconnect.php
@@ -5,12 +5,12 @@ if (isset($_POST['fname'])) {
     $password = "";
     $database = "users";
 
-    $conn =  mysqli_connect($server, $username, $password, $database);
+    /*$conn =  mysqli_connect($server, $username, $password, $database);
     if ($conn) {
         echo "success! you connected with the database";
     } else {
         die("Error" . mysqli_connect_error());
-    }
+    }*/
     $fname = $_POST['fname'];
     $lname = $_POST['lname'];
     $studentID = $_POST['studentID'];
@@ -19,8 +19,12 @@ if (isset($_POST['fname'])) {
     $mobile = $_POST['mobile'];
     $email = $_POST['email'];
     $password = $_POST['password'];
-
-
+    //Decode the JSON
+    //Make a new associative array of new member's information.
+    //place it in the array that will be using json_decode("users.json")
+    //json_encode("users.json") to place new member inside json file correctly
+    //Might have to use file_put_contents(). Ask TA!
+    $json_a=array("StudentID"=$_POST['studentID'],)
     $json_obj->fname = $fname;
     $json_obj->lname = $lname;
     $json_obj->studentID = $studentID;
@@ -36,10 +40,10 @@ if (isset($_POST['fname'])) {
     // $sql = "INSERT INTO `users`.`users` (`firstname`, `lastname`, `studentID`,`address`, `city`, `mobile`, `email`, `password`, `dt`) VALUES ('$fname', '$lname', '$studentID','$address', '$city', '$mobile', '$email', '$password',currenttimestamp())";
     // echo $sql;
 
-    if ($conn->query($sql) == true) {
+    /*if ($conn->query($sql) == true) {
         echo " successfully Inserted!";
     } else {
         echo "ERROR: $sql <br> $conn->error";
     }
-    $conn->close();
+    $conn->close();*/
 }
