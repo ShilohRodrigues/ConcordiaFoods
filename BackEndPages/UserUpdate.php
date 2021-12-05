65132d39c6018fea86082509541d1aab7b875744
AdamFarahat <91172205+AdamFarahat@users.noreply.github.com>
2021-12-05 16:01:23 -0500

    Create UserUpdate.php

---
+++ BackEndPages/UserUpdate.php
@@ -0,0 +1,18 @@
+<?php
+  echo "New user added";
+  $users=file_get_contents('Databases/users.json');
+  $userArray = json_decode($users);
+      $arr = array(
+            'StudentID' => $_POST['sID'],
+            'First_name' => $_POST['fName'],
+            'Last_name' => $_POST['lName'],
+            'Password' => $_POST['Password'],
+            'E-mail' => $_POST['email'],
+            'P_Number' => $_POST['Phone Number'],
+            'M_Number' => $_POST['mobile'],
+            'Address' => $_POST['Address']);
+      $newUser[]=$arr;
+      array_push($userArray,$newUser);
+      $json_data=json_encode($userArray);
+      file_put_contents('Databases/users.json',$json_data);
+ ?>
