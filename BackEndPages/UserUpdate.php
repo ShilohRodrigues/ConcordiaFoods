3cb845905be8ba2fbb40cc6b308c2beeacfd05e0
AdamFarahat <91172205+AdamFarahat@users.noreply.github.com>
2021-12-06 12:08:00 -0500

    Update UserUpdate.php

--- BackEndPages/UserUpdate.php
+++ BackEndPages/UserUpdate.php
@@ -1,7 +1,9 @@
 <?php
-  echo "New user added";
+  $new=false;
   $users=file_get_contents('Databases/users.json');
-  $userArray = json_decode($users);
+  $userArray = json_decode($users,true);
+  $l=count($userArray);
+  $json_data;
       $arr = array(
             'StudentID' => $_POST['sID'],
             'First_name' => $_POST['fName'],
@@ -11,7 +13,36 @@
             'P_Number' => $_POST['Pnumber'],
             'M_Number' => $_POST['mobile'],
             'Address' => $_POST['Address']);
+      for($i=0; $i<($l);$i++){
+        if(strcmp($userArray[$i]['StudentID'],$arr['StudentID'])==0){
+          $userArray[$i] = $arr;
+          $json_data=json_encode($userArray,JSON_PRETTY_PRINT);
+          file_put_contents('Databases/users.json',$json_data);
+          break;
+      }
+      elseif($i==count($userArray)-1){
+        $new=true;
+      }
+      }
+      if($new){
+        $userArray[] = $arr;
+        $json_data=json_encode($userArray,JSON_PRETTY_PRINT);
+        file_put_contents('Databases/users.json',$json_data);
+      }
+  header('location:UsersList.php');
+
+  /*$users=file_get_contents('Databases/users.json');
+  $userArray = json_decode($users);
+      $arr = array(
+            'StudentID'=>$_POST['sID'],
+            'First_name' => $_POST['fName'],
+            'Last_name' => $_POST['lName'],
+            'Password' => $_POST['Password'],
+            'E-mail' => $_POST['email'],
+            'P_Number' => $_POST['Pnumber'],
+            'M_Number' => $_POST['mobile'],
+            'Address' => $_POST['Address']);
       $userArray[] = $arr;
       $json_data=json_encode($userArray,JSON_PRETTY_PRINT);
-      file_put_contents('Databases/users.json',$json_data);
+      file_put_contents('Databases/users.json',$json_data);*/
  ?>
